f2e452c914c847902576ec30a2d42b3c9a469cf2
Alberto Zaccagni <alberto.zaccagni@lazywithclass.com>
2013-07-16 15:58:59 +0200

    Open recent files

--- emacs.d/keys-mappings.el
+++ emacs.d/keys-mappings.el
@@ -1,6 +1,8 @@
 (global-set-key (kbd "C-x f") 'find-file-in-repository)
 (global-set-key (kbd "C-x C-c") 'ido-switch-buffer)
 (global-set-key (kbd "C-x C-a") 'iedit-mode)
+(global-set-key (kbd "C-x C-r") 'recentf-open-files)
+
 (global-set-key (kbd "C-S-w") 'duplicate-line)
 (global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region-or-line)
 
