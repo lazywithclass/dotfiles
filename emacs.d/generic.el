a4d5ae5bdfa6768be1c9d99b7b5957ad4652d18c
Alberto Zaccagni <alberto.zaccagni@lazywithclass.com>
2013-08-20 23:57:56 +0200

    Apparently these were useless, it's enough to launch emacs from the command line

--- emacs.d/generic.el
+++ emacs.d/generic.el
@@ -5,13 +5,6 @@
     (byte-compile-file buffer-file-name)))
 (add-hook 'after-save-hook 'byte-compile-current-buffer)
 
-(let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
-  (setenv "PATH" path)
-  (setq exec-path
-        (append
-         (split-string-and-unquote path ":")
-         exec-path)))
-
 (setq default-directory "~/workspace/" )
 
 ;; use git as backup not ~ files
@@ -78,16 +71,6 @@
 )
 
 (color-theme-solarized-dark)
-
-(defun set-exec-path-from-shell-PATH ()
-  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
-
-This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
-  (interactive)
-  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
-(set-exec-path-from-shell-PATH)
 
 (desktop-save-mode 1)
 
