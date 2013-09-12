7f10f3751f5ee77fe8f946b514de58ea929134fc
Alberto Zaccagni <alberto.zaccagni@lazywithclass.com>
2013-09-12 17:52:13 +0200

    Spotify keybindings

--- emacs.d/keys-mappings.el
+++ emacs.d/keys-mappings.el
@@ -1,12 +1,16 @@
 (global-set-key (kbd "C-x f") 'find-file-in-repository)
 (global-set-key (kbd "C-x C-c") 'ido-switch-buffer)
-(global-set-key (kbd "C-x C-a") 'iedit-mode)
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
 
 (global-set-key (kbd "C-S-w") 'duplicate-line)
 (global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region-or-line)
-
 (global-set-key (kbd "C-c m") 'run-suite)
+(global-set-key (kbd "C-c t") 'tern-get-type)
+(global-set-key (kbd "C-c C-a") 'iedit-mode)
+
+(global-set-key (kbd "C-c s p") 'spotify-previous)
+(global-set-key (kbd "C-c s t") 'spotify-toggle)
+(global-set-key (kbd "C-c s n") 'spotify-next)
 
 (global-set-key (kbd "M-<left>") 'windmove-left)
 (global-set-key (kbd "M-<right>") 'windmove-right)
