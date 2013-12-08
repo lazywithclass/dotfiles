500ba65aa6d8b5f16ac0e20bc12317b768985b08
Alberto Zaccagni <alberto.zaccagni@lazywithclass.com>
2013-12-08 21:24:08 +0100

    Added a function to reload every configuration

--- emacs.d/generic.el
+++ emacs.d/generic.el
@@ -79,3 +79,7 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
+
+(defun reload-init ()
+  (interactive)
+  (load-file "/workspace/dotfiles/emacs.d/init.el"))
