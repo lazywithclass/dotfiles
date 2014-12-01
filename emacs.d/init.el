12c65c865d645f78b3fcbb20d728cc8d682cdbfd
lazywithclass <montecristoh@gmail.com>
2014-12-01 13:41:35 +0000

    added coffee-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -16,7 +16,9 @@
 
 (setq my-el-get-packages
       (append  
-       '(css-mode js2-mode auto-complete fiplr flymake-easy grizzl multi-web-mode popup s fastnav color-theme-solarized evil yasnippet js2-refactor)
+       '(css-mode js2-mode auto-complete fiplr flymake-easy grizzl multi-web-mode
+                  popup s fastnav color-theme-solarized evil yasnippet js2-refactor
+                  coffee-mode)
        (mapcar 'el-get-source-name el-get-sources)))  
   
 (el-get 'sync my-el-get-packages)  
