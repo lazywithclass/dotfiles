e67a45cff5d70abe90b336565b0be425d9e2785e
lazywithclass <montecristoh@gmail.com>
2014-12-16 21:05:26 +0000

    generalising homeh

--- emacs.d/elisp/javascript.el
+++ emacs.d/elisp/javascript.el
@@ -6,7 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.?js\\'" . js2-mode))
 (add-hook 'js2-mode-hook (lambda () (slime-js-minor-mode 1)))
 
-(add-to-list 'load-path "/home/lazywithclass/tern/emacs")
+(add-to-list 'load-path "~/tern/emacs")
 (autoload 'tern-mode "tern.el" nil t)
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 (eval-after-load 'tern
