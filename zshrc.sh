97be13ce13f5ac253c8b15c825e6d44a2d1b4b11
lazywithclass <montecristoh@gmail.com>
2014-12-24 20:40:24 +0000

    removed vi-mode as it does not really work as I intended

--- zshrc.sh
+++ zshrc.sh
@@ -13,7 +13,7 @@ ZSH_THEME="robbyrussell"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(vi-mode)
+plugins=()
 
 source $ZSH/oh-my-zsh.sh
 
