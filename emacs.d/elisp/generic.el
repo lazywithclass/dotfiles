d481ddaedf4b443b87319c75f30d2d158fd6dea5
lazywithclass <montecristoh@gmail.com>
2015-01-01 22:41:01 +0000

    I dont have ruby support anymore

--- emacs.d/elisp/generic.el
+++ emacs.d/elisp/generic.el
@@ -47,7 +47,7 @@
 (require 'auto-complete-config)
 (ac-config-default)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-(add-to-list 'ac-modes '(emacs-lisp-mode ruby-mode))
+(add-to-list 'ac-modes '(emacs-lisp-mode))
 
 (setq gc-cons-threshold 20000000)
 
