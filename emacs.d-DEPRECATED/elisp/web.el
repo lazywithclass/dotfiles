bb2a83b9cee5263bdd36d65c1f7fbec4ef92c700
lazywithclass <montecristoh@gmail.com>
2016-10-10 22:30:25 +0100

    not used

--- emacs.d-DEPRECATED/elisp/web.el
+++
@@ -1,7 +0,0 @@
-(require 'multi-web-mode)
-(setq mweb-default-major-mode 'html-mode)
-(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-                  (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
-(setq mweb-filename-extensions '("htm" "html"))
-(multi-web-global-mode 1)
