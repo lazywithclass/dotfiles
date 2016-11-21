af62deee5b43561df1094d2140c7d7f4a87aac4c
lazywithclass <montecristoh@gmail.com>
2016-11-21 18:10:55 +0000

    added project name in the header

--- spacemacs/private-layers/head-line/funcs.el
+++ spacemacs/private-layers/head-line/funcs.el
@@ -1,7 +1,11 @@
-(defun display-buffer-path()
+(defun buffer-path()
   (defconst file-path (buffer-file-name (window-buffer (minibuffer-selected-window))))
   (defconst project-path (projectile-locate-dominating-file file-path ".git"))
   (s-replace project-path "" file-path))
+
+(defun project-name()
+  (defconst project-path (projectile-locate-dominating-file file-path ".git"))
+  (car (last (s-split-words project-path))))
 
 (set-face-attribute 'header-line nil
                     :inherit nil
@@ -10,4 +14,4 @@
                     :underline "#5D4D7A")
 
 (setq-default
- header-line-format '(:eval (display-buffer-path)))
+ header-line-format '(:eval (concat (project-name) " | " (buffer-path))))
